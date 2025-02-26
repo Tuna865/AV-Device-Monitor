e361f821c9d5ab4c5b751859b8158331fa552de2
Turner <turnermilan@m3techgroup.com>
2025-02-25 20:22:24 -0500

    started work on user input & improvements

--- Program.cs
+++ Program.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Net.Http;
-using System.Runtime.InteropServices.ComTypes;
-using System.Runtime.InteropServices.JavaScript;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
+﻿using System.Text;
 using System.Text.Json;
-using System.Timers;
 using Newtonsoft.Json.Linq;
 
 
@@ -13,15 +7,12 @@ using Newtonsoft.Json.Linq;
 public class Program
 {
     private static readonly HttpClient client = new();
-    const string psk = "8657";          //would like to get PSK and IP from user input 
-    const string ip = "10.0.0.198";     //the Sony display used for testing had its IP set to static
     const string powerMethod = "getPowerStatus";
     const string infoMethod = "getSystemInformation";
     const string timeMethod = "getCurrentTime"; 
     const int powerId = 50;
     const int infoId = 33;
     const int timeId = 51;
-    const string url = $"http://{ip}/sony/system";
 
     public class JsonRequest
     {
@@ -31,23 +22,54 @@ public class Program
         public string? version { get; set; }
     }
 
+    public class ConnectionInfo  
+    {
+        public static string AskUserForIP()
+        {
+            Console.Write("Enter Device IP Address using the format xxx.xxx.xxx.xxx  ");
+            string ip = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(ip))
+            {
+                Console.WriteLine("Please enter a valid IP address.");
+                return null;
+            }
+            else return ip;
+        }public static string AskUserForPSK()
+        {
+            Console.Write("Enter 4 Digit Pre-Shared Key:  ");
+            string psk = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(psk))
+            {
+                Console.WriteLine("Please enter a valid 4 digit Pre-Shared Key.");
+                return null;
+            }
+            else return psk;
+        }
+
+
+    }
+
     public static void Main()
     {
-        
         try
         {
             Console.WriteLine("AV-Device-Monitor start \n");
-            Send(infoMethod, infoId);   //collect and show device info upon startup 
+            string ip = ConnectionInfo.AskUserForIP();
+            string psk = ConnectionInfo.AskUserForPSK();
+            string baseURL = $"http://{ip}/sony/system";
+            Console.WriteLine($"Connection info:\n\t {ip}\n\t {psk}\n\t {baseURL}");
+
+            Send(infoMethod, infoId, baseURL, psk);   //collect and show device info upon startup 
             StartPowerPoll();
             StartTimeCheck();
-            Console.ReadLine();        //make the program wait instead of exiting
+            Console.ReadLine();     
         }
         catch (Exception ex)
         {
             Console.WriteLine("Error in Main: " + ex.Message);
         }
     }
-    
+
     //checks time on the AV Device every 5 minutes 
     private static System.Timers.Timer? statusTimer;
     public static void StartTimeCheck()
@@ -69,7 +91,7 @@ public class Program
     }
 
     //sends and interprets JSON from Sony's REST API
-    public static void Send(string method, int id)         
+    public static void Send(string method, int id, string url = "", string psk = "")         
     {
         try
         {
@@ -86,57 +108,101 @@ public class Program
                 Encoding.UTF8,
                 "application/json"
             );
-
+            Console.WriteLine($"Sending info to TV:\n\t URL: {url}\n\t PSK: {psk}\n" );
             client.DefaultRequestHeaders.Add("X-Auth-PSK", psk);                 //this authentication is required to use Sony's REST API control  
             var response = client.PostAsync(url, jsonContent).Result;
+            Console.WriteLine(response);
             response.EnsureSuccessStatusCode();
-
+            
             var jsonResponse = response.Content.ReadAsStringAsync().Result;
             JObject jsonResult = JObject.Parse(jsonResponse);
-                if (id == 33)    //device info
-                {
+                //if (id == 33)    //device info
+                //{
+                //    var resultToken = jsonResult.GetValue("result").First;     //the TV returns a lot more than what we need, so we need to select what to display
+   
+                //    var product = $"Device Type: { resultToken.SelectToken("product") }";
+                //    var model = $"Model: { resultToken.SelectToken("model") }";
+                //    var serialNumber = $"Serial Number: { resultToken.SelectToken("serial") }";
+                //    var MACAddress = $"MAC Address: { resultToken.SelectToken("macAddr") }";
+                //    var name = $"Name: { resultToken.SelectToken("name") }";
+                
+                //    var avDeviceInfo = $"AV Device Information: \n {product} \n {model} \n {serialNumber} \n {MACAddress} \n {name} \n";
+                //    Console.WriteLine(avDeviceInfo);
+
+                //};
+
+                //if (id == 50)    //power status
+                //{
+                //    if (jsonResponse.Contains("active")) { Console.WriteLine("AV Device is active"); }; 
+                //    if (jsonResponse.Contains("standby")) { Console.WriteLine("AV Device is in standby mode"); };
+                //};
+
+                //if (id == 51)      //current time
+                //{
+                //    var timeToken = jsonResult.GetValue("result").First;
+                //    var deviceTime = ($"{timeToken}").Substring(11, 4);        //returns the full date & time but we just want HH:MM
+
+                //    DateTime dateTime = DateTime.Now;
+                //    var localMachineDateTime = dateTime.ToString();
+                //    var localTime = localMachineDateTime.Substring(11,4);
+
+                //    if (deviceTime != localTime | deviceTime == null)         //compare the device being monitored to the machine running the program; if different time zone, minutes should still be the same 
+                //    {
+                //        Console.WriteLine("AV Device time is out of sync. Check device network connection and/or settings.");
+                //        Console.WriteLine($"Device { deviceTime} != Local { localTime}");
+                //    }
+                //    if (deviceTime == localTime)
+                //    {
+                //        Console.WriteLine($"Time Check Successful: Device {deviceTime} = Local {localTime}");        
+                //    }
+                //}
+            switch (id)
+            {
+                case 33:
                     var resultToken = jsonResult.GetValue("result").First;     //the TV returns a lot more than what we need, so we need to select what to display
 
-                    var product = $"Device Type: { resultToken.SelectToken("product") }";
-                    var model = $"Model: { resultToken.SelectToken("model") }";
-                    var serialNumber = $"Serial Number: { resultToken.SelectToken("serial") }";
-                    var MACAddress = $"MAC Address: { resultToken.SelectToken("macAddr") }";
-                    var name = $"Name: { resultToken.SelectToken("name") }";
-                
+                    var product = $"Device Type: {resultToken.SelectToken("product")}";
+                    var model = $"Model: {resultToken.SelectToken("model")}";
+                    var serialNumber = $"Serial Number: {resultToken.SelectToken("serial")}";
+                    var MACAddress = $"MAC Address: {resultToken.SelectToken("macAddr")}";
+                    var name = $"Name: {resultToken.SelectToken("name")}";
+
                     var avDeviceInfo = $"AV Device Information: \n {product} \n {model} \n {serialNumber} \n {MACAddress} \n {name} \n";
                     Console.WriteLine(avDeviceInfo);
 
-                };
+                    break;
 
-                if (id == 50)    //power status
-                {
-                    if (jsonResponse.Contains("active")) { Console.WriteLine("AV Device is active"); }; 
+                case 50:
+                    if (jsonResponse.Contains("active")) { Console.WriteLine("AV Device is active"); };
                     if (jsonResponse.Contains("standby")) { Console.WriteLine("AV Device is in standby mode"); };
-                };
+                    break;
 
-                if (id == 51)      //current time
-                {
+                case 51:
                     var timeToken = jsonResult.GetValue("result").First;
                     var deviceTime = ($"{timeToken}").Substring(11, 4);        //returns the full date & time but we just want HH:MM
 
                     DateTime dateTime = DateTime.Now;
                     var localMachineDateTime = dateTime.ToString();
-                    var localTime = localMachineDateTime.Substring(11,4);
+                    var localTime = localMachineDateTime.Substring(11, 4);
 
                     if (deviceTime != localTime | deviceTime == null)         //compare the device being monitored to the machine running the program; if different time zone, minutes should still be the same 
                     {
                         Console.WriteLine("AV Device time is out of sync. Check device network connection and/or settings.");
-                        Console.WriteLine($"Device { deviceTime} != Local { localTime}");
+                        Console.WriteLine($"Device {deviceTime} != Local {localTime}");
                     }
                     if (deviceTime == localTime)
                     {
-                        Console.WriteLine($"Time Check Successful: Device {deviceTime} = Local {localTime}");        
+                        Console.WriteLine($"Time Check Successful: Device {deviceTime} = Local {localTime}");
                     }
-                }
+
+                    break;
+            }
+
+
         }  
         catch (Exception ex)
         {
-            Console.WriteLine($"Communication with AV Device error.\n {ex.Message}");
+            Console.WriteLine($"Communication with AV Device error.\n\t {ex.Message}");
         }
     }
 }
