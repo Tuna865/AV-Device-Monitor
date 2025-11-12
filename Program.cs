d13b4c86f6878d5650a94fd15bc555376c653f09
Turner Milan <turnermilan@gmail.com>
2025-11-11 20:27:53 -0600

    cleaned up program a little

--- Program.cs
+++ Program.cs
@@ -3,18 +3,17 @@ using System.Text.Json;
 using Newtonsoft.Json.Linq;
 
 
-
 public class Program
 {
-    private static readonly HttpClient client = new();
-    const string powerMethod = "getPowerStatus";
-    const string infoMethod = "getSystemInformation";
-    const string timeMethod = "getCurrentTime"; 
-    const int powerId = 50;
-    const int infoId = 33;
-    const int timeId = 51;
+    private static readonly HttpClient _client = new();
+    private const string _powerMethod = "getPowerStatus";
+    private const string _infoMethod = "getSystemInformation";
+    private const string _timeMethod = "getCurrentTime";
+    private const int _powerId = 50;
+    private const int _infoId = 33;
+    private const int _timeId = 51;
 
-    public class JsonRequest
+    public class RpcRequest
     {
         public string? method { get; set; }
         public int? id { get; set; }         
@@ -24,20 +23,21 @@ public class Program
 
     public class ConnectionInfo  
     {
-        public static string AskUserForIP()
+        public static string? AskUserForIP()
         {
             Console.Write("Enter Device IP Address using the format xxx.xxx.xxx.xxx  ");
-            string ip = Console.ReadLine();
+            string? ip = Console.ReadLine();
             if (string.IsNullOrWhiteSpace(ip))
             {
                 Console.WriteLine("Please enter a valid IP address.");
                 return null;
             }
             else return ip;
-        }public static string AskUserForPSK()
+        }
+        public static string? AskUserForPSK()
         {
             Console.Write("Enter 4 Digit Pre-Shared Key:  ");
-            string psk = Console.ReadLine();
+            string? psk = Console.ReadLine();
             if (string.IsNullOrWhiteSpace(psk))
             {
                 Console.WriteLine("Please enter a valid 4 digit Pre-Shared Key.");
@@ -45,8 +45,6 @@ public class Program
             }
             else return psk;
         }
-
-
     }
 
     public static void Main()
@@ -54,12 +52,12 @@ public class Program
         try
         {
             Console.WriteLine("AV-Device-Monitor start \n");
-            string ip = ConnectionInfo.AskUserForIP();
-            string psk = ConnectionInfo.AskUserForPSK();
+            string? ip = ConnectionInfo.AskUserForIP();
+            string? psk = ConnectionInfo.AskUserForPSK();
             string baseURL = $"http://{ip}/sony/system";
             Console.WriteLine($"Connection info:\n\t {ip}\n\t {psk}\n\t {baseURL}");
 
-            Send(infoMethod, infoId, baseURL, psk);   //collect and show device info upon startup 
+            Send(_infoMethod, _infoId, baseURL, psk);   //collect and show device info upon startup 
             StartPowerPoll();
             StartTimeCheck();
             Console.ReadLine();     
@@ -70,32 +68,31 @@ public class Program
         }
     }
 
-    //checks time on the AV Device every 5 minutes 
+    // time  
     private static System.Timers.Timer? statusTimer;
     public static void StartTimeCheck()
     {
         statusTimer = new System.Timers.Timer(300000);
-        statusTimer.Elapsed += (sender, e) => Send(timeMethod, timeId);
+        statusTimer.Elapsed += (sender, e) => Send(_timeMethod, _timeId);
         statusTimer.AutoReset = true;
         statusTimer.Start();
     }
 
-    //polls power status 
+    // power status 
     private static System.Timers.Timer? powerTimer;
     public static void StartPowerPoll()
     {
         powerTimer = new System.Timers.Timer(3000);
-        powerTimer.Elapsed += (sender, e) => Send(powerMethod, powerId);
+        powerTimer.Elapsed += (sender, e) => Send(_powerMethod, _powerId);
         powerTimer.AutoReset = true;
         powerTimer.Start();
     }
 
-    //sends and interprets JSON from Sony's REST API
     public static void Send(string method, int id, string url = "", string psk = "")         
     {
         try
         {
-            var requestBody = new JsonRequest
+            var requestBody = new RpcRequest
             {
                 method = method,
                 id = id,          
@@ -109,8 +106,8 @@ public class Program
                 "application/json"
             );
             Console.WriteLine($"Sending info to TV:\n\t URL: {url}\n\t PSK: {psk}\n" );
-            client.DefaultRequestHeaders.Add("X-Auth-PSK", psk);                 //this authentication is required to use Sony's REST API control  
-            var response = client.PostAsync(url, jsonContent).Result;
+            _client.DefaultRequestHeaders.Add("X-Auth-PSK", psk);                 // this authentication is required to use Sony's REST API control  
+            var response = _client.PostAsync(url, jsonContent).Result;
             Console.WriteLine($"Response from Sony REST API: {response}");
             response.EnsureSuccessStatusCode();
             
@@ -120,13 +117,13 @@ public class Program
             switch (id)
             {
                 case 33:
-                    var resultToken = jsonResult.GetValue("result").First;     //the TV returns a lot more than what we need, so we need to select what to display
+                    var resultToken = jsonResult.GetValue("result")?.First;     //the TV returns a lot more than what we need, so we need to select what to display
 
-                    var product = $"Device Type: {resultToken.SelectToken("product")}";
-                    var model = $"Model: {resultToken.SelectToken("model")}";
-                    var serialNumber = $"Serial Number: {resultToken.SelectToken("serial")}";
-                    var MACAddress = $"MAC Address: {resultToken.SelectToken("macAddr")}";
-                    var name = $"Name: {resultToken.SelectToken("name")}";
+                    var product = $"Device Type: {resultToken?.SelectToken("product")}";
+                    var model = $"Model: {resultToken?.SelectToken("model")}";
+                    var serialNumber = $"Serial Number: {resultToken?.SelectToken("serial")}";
+                    var MACAddress = $"MAC Address: {resultToken?.SelectToken("macAddr")}";
+                    var name = $"Name: {resultToken?.SelectToken("name")}";
 
                     var avDeviceInfo = $"AV Device Information: \n {product} \n {model} \n {serialNumber} \n {MACAddress} \n {name} \n";
                     Console.WriteLine(avDeviceInfo);
@@ -139,14 +136,14 @@ public class Program
                     break;
 
                 case 51:
-                    var timeToken = jsonResult.GetValue("result").First;
-                    var deviceTime = ($"{timeToken}").Substring(11, 4);        //returns the full date & time but we just want HH:MM
+                    var timeToken = jsonResult.GetValue("result")?.First;
+                    var deviceTime = ($"{timeToken}").Substring(11, 4);        // we just want HH:MM
 
                     DateTime dateTime = DateTime.Now;
                     var localMachineDateTime = dateTime.ToString();
                     var localTime = localMachineDateTime.Substring(11, 4);
 
-                    if (deviceTime != localTime | deviceTime == null)         //compare the device being monitored to the machine running the program; if different time zone, minutes should still be the same 
+                    if (deviceTime != localTime | deviceTime == null)         //
                     {
                         Console.WriteLine("AV Device time is out of sync. Check device network connection and/or settings.");
                         Console.WriteLine($"Device {deviceTime} != Local {localTime}");
